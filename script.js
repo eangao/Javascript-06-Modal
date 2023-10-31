40774e731dc883214b1505fcb0d9e3399cce5f2b
elmar <elmarcera@gmail.com>
2023-10-31 16:43:38 +0800

    PROJECT #2: Modal Window

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+////////////////////////////////////////////////////////
+// PROJECT #2: Modal Window
+////////////////////////////////////////////////////////
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModal);
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i]);
+}
