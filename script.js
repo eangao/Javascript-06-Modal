cc7a7aadcacc23085e8332d81424fbcb6ae1555c
elmar <elmarcera@gmail.com>
2023-10-31 17:08:01 +0800

    Working With Classes

--- script.js
+++ script.js
@@ -6,12 +6,28 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnOpenModal);
+const openModal = function () {
+  console.log('Button clicked');
+
+  //not use the dot ex. .hidden.  The dot is only for the selector. We are passing here the name of the class
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i]);
+  // console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', openModal);
 }
+
+// do not call the function right away thats why we don't add  parentheses  ex.closeModal(). call the function only if the button is click
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
