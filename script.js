fbacd4db8d0219f45462cc51d1aca67b3fb752a0
elmar <elmarcera@gmail.com>
2023-11-03 09:12:43 +0800

    Handling an 'Esc' Keypress Event

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked');
+  // console.log('Button clicked');
 
   //not use the dot ex. .hidden.  The dot is only for the selector. We are passing here the name of the class
   modal.classList.remove('hidden');
@@ -31,3 +31,18 @@ for (let i = 0; i < btnOpenModal.length; i++) {
 // do not call the function right away thats why we don't add  parentheses  ex.closeModal(). call the function only if the button is click
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// there are actually three types of events for the keyboard.
+// There is the key down the key press or the key up.
+
+// But this time we actually need to look
+// at the event object in order to figure out
+// which key was pressed.
+// And how can we do that?
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
